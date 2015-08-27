9b0612a83f829d6996db4b47d64b5ebe8914c5f7
nischaalc <nc475@drexel.edu>
2015-08-26 22:04:56 -0400

    FINALLY FIXED GOOGLE

--- JS/main.js
+++ JS/main.js
@@ -9,13 +9,13 @@ function onSuccess(googleUser) {
 
 function renderButton() {
     "use strict";
-    gapi.signin2.render('my-signin2', {
+    gapi.signin2.render('googleIn', {
         'scope': 'https://www.googleapis.com/auth/calendar profile',
         'width': 200,
         'height': 50,
         'longtitle': true,
         'theme': 'dark',
         'cookiepolicy': 'single_host_origin',
-        'onsuccess': onSuccess,
+        'onsuccess': onSuccess
     });
 }
