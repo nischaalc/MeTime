85850976e64c1a649d026662de53093605b67ed6
nischaalc <nc475@drexel.edu>
2015-08-26 20:44:35 -0400

    Lotsa big changes

--- JS/calendar.js
+++ JS/calendar.js
@@ -1,77 +1,102 @@
+var allEvents = [
+            {
+                title: 'Long Event',
+                start: '2015-08-10',
+                end: '2015-08-26'
+            },
+            {
+                id: 999,
+                title: 'Repeating Event',
+                start: '2015-08-26T16:00:00',
+                end: '2015-08-26T18:00:00'
+            },
+            {
+                title: 'Conference',
+                start: '2015-08-25',
+                end: '2015-08-27'
+            },
+            {
+                title: 'Lunch',
+                start: '2015-08-25T12:00:00',
+                end: '2015-08-25T14:00:00'
+            },
+            {
+                title: 'Happy Hour',
+                start: '2015-08-24T17:30:00',
+                end: '2015-08-24T19:00:00'
+            },
+            {
+                title: 'Dinner',
+                start: '2015-08-26T20:00:00',
+                end: '2015-08-26T22:00:00'
+            },
+            {
+                title: 'Birthday Party',
+                start: '2015-08-27T07:00:00',
+                end: '2015-08-27T09:00:00'
+            },
+            {
+                title: 'Click for Google',
+                url: 'http://google.com/',
+                start: '2015-08-28',
+                end: '2015-08-29'
+            }
+        ];
+
 $(window).load(function () {
     "use strict";
-    var height = ($(window).height() / 1.5);
+
+    $('#calendar').css('visibility', 'hidden');
+    $('#dayinfo').hide();
+    $('#footer').hide();
     
+    createCalendar();
+    
+    $('.spinner').fadeOut(750, function() {
+        $('#calendar').fadeIn(function() {
+            $('#calendar').css('visibility', 'visible');
+        });
+        $('#dayinfo').fadeIn();
+        $('#footer').fadeIn();
+    });        
+        
+    var name = getUrlVars()['name'];
+    name = name.replace('%20', ' ');
+    $('#username').text(name);
+    
+});
+
+function createCalendar() {
+    var height = ($(window).height() / 1.5);
+
+    var day = moment().format('e');
+    if (day == 7)
+        day = 0;
     $('#calendar').fullCalendar({
         header: {
             left: 'prev',
             center: 'title',
             right: 'next'
         },
-        firstDay: 1,
+        firstDay: day,
         defaultView: 'basicWeek',
         height: height,
-        events: [
-            {
-                title: 'All Day Event',
-                start: '2015-02-01'
-            },
-            {
-                title: 'Long Event',
-                start: '2015-08-10',
-                end: '2015-08-11'
-            },
-            {
-                id: 999,
-                title: 'Repeating Event',
-                start: '2015-08-12T16:00:00'
-            },
-            {
-                id: 999,
-                title: 'Repeating Event',
-                start: '2015-08-16T16:00:00'
-            },
-            {
-                title: 'Conference',
-                start: '2015-08-11',
-                end: '2015-08-13'
-            },
-            {
-                title: 'Meeting',
-                start: '2015-08-13T10:30:00',
-                end: '2015-08-13T12:30:00'
-            },
-            {
-                title: 'Lunch',
-                start: '2015-08-15T12:00:00'
-            },
-            {
-                title: 'Meeting',
-                start: '2015-02-12T14:30:00'
-            },
-            {
-                title: 'Happy Hour',
-                start: '2015-08-114T17:30:00'
-            },
-            {
-                title: 'Dinner',
-                start: '2015-08-115T20:00:00'
-            },
-            {
-                title: 'Birthday Party',
-                start: '2015-08-13T07:00:00'
-            },
-            {
-                title: 'Click for Google',
-                url: 'http://google.com/',
-                start: '2015-08-12'
-            }
-        ]
-    });
-    var name = getUrlVars()['name'];
-    name = name.replace('%20', ' ');
-    $('#username').text(name);
-});
+        events: allEvents,
+        dayClick: function(date, jsEvent, view) {
+            getLocation(date);
+        },
+        loading: function(isLoading, view) {
+            if (isLoading) 
+                $('.spinner').show();
+            else 
+                $('.spinner').hide();
+        }
+    }); 
+}
+
+function addEvent() {
+    console.log('test');
+}
 
 function getUrlVars() {
     "use strict";
@@ -85,3 +110,50 @@ function getUrlVars() {
     }
     return vars;
 }
+
+function getLocation(date) {
+    var url = 'http://api.wunderground.com/api/a13f71c3ae3eecbb/geolookup/q/autoip.json';
+    
+    $.getJSON(url)
+        .done(function(data) {
+            getWeather(data.location.zip, date);
+            $('#dayinfo').fadeIn(500);
+    });
+}
+
+function getWeather(zip, date) {
+    var url =  'http://api.wunderground.com/api/a13f71c3ae3eecbb/forecast10day/q/' + zip + '.json';  
+    $.getJSON(url).done(function(data) {
+        var forecastArray = data.forecast.simpleforecast.forecastday;
+        var icon, high, low, desc;
+        var dayOfWeek = date.format('dddd');
+        for (var i = 0; i < 7; i++) {
+            if (forecastArray[i].date.weekday == dayOfWeek) {
+                icon = forecastArray[i].icon_url;
+                desc = forecastArray[i].conditions;
+                high = forecastArray[i].high.fahrenheit;
+                low = forecastArray[i].low.fahrenheit;
+                displayWeather(icon, desc, high, low);
+            }
+        }
+    });
+}
+
+function displayWeather(icon, desc, high, low) {
+    var strHtml = '<img src=' + icon + ' class="icon"/>';
+    strHtml = strHtml + '<p class="temp">High: ' + high + ' &deg;F</p>';
+    strHtml = strHtml + '<p class="temp">Low: ' + low + ' &deg;F</p>';
+    strHtml = strHtml + '<p class="desc">' + desc + '</p>';
+    
+    $('.weatherobject').html(strHtml);
+}
+
+$(function() {
+    var htmlString = '<div class="modal"><p>Test</p></div>'
+    $('.link').click(function() {
+        vex.dialog.open({
+            message: 'Test',
+            input: htmlString
+        });
+    });
+});
