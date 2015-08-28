9e045081304284ce3fe7f0ac7c879730f58116cc
nischaalc <nc475@drexel.edu>
2015-08-28 17:56:34 -0400

    More...

--- JS/main.js
+++ JS/main.js
@@ -2,8 +2,9 @@ var clientID = '593421187274-6oq2kl1u02u4k97lps8vu2tdmpgofpep.apps.googleusercon
 
 function onSuccess(googleUser) {
     "use strict";
-    window.location.href = "http://nischaalc.github.io/MeTime/users?name=" + googleUser.getBasicProfile().getName();
+    //window.location.href = "http://nischaalc.github.io/MeTime/users?name=" + googleUser.getBasicProfile().getName();
     window.localStorage.setItem("googleUser.object", googleUser.getAuthResponse());
+    console.log(googleUser.getAuthResponse()['access_token'];
 }
 
 function renderButton() {
