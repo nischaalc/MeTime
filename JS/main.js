f23860f03b10ac9703bff358e416dc82d8f1ab7b
nischaalc <nc475@drexel.edu>
2015-08-28 16:52:53 -0400

    Testing testing 123

--- JS/main.js
+++ JS/main.js
@@ -3,9 +3,10 @@ var clientID = '593421187274-6oq2kl1u02u4k97lps8vu2tdmpgofpep.apps.googleusercon
 function onSuccess(googleUser) {
     "use strict";
     console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
-    window.location.href = "http://nischaalc.github.io/MeTime/users?name=" + googleUser.getBasicProfile().getName();
+    //window.location.href = "http://nischaalc.github.io/MeTime/users?name=" + googleUser.getBasicProfile().getName();
+    console.log(googleUser);
     console.log(googleUser.getBasicProfile());
-    //window.localStorage.setItem("googleUser.object", googleUser);
+    window.localStorage.setItem("googleUser.object", googleUser);
 }
 
 function renderButton() {
